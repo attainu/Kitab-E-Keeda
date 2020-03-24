92b213c57502d2b2f692f0cd2504a8bd7c6949e0
Manas-Sahoo-au6 <rmanas000@gmail.com>
2020-03-23 18:02:54 -0700

    server connected

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 const express = require('express');
-var dotenv = require("dotenv")
-dotenv.config();
 
-require('./db');
+var dotenv = require("dotenv");
+dotenv.config();
+require("./db");
+
 const app = express();
+var morgan     = require('morgan');
+
+// configure app
+app.use(morgan('dev')); // log requests to the console
+
+// configure express body parser
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+var port  = process.env.PORT || 8080; // set our port
+
+app.get('/', (req, res) => {
+  res.send([{
+    name1:"amit",
+    Name2 : "Manas"
+
+  }]);
+});
+
+app.listen(port);
+console.log(`Port is Listening Magic happens on port ${port}`);
+
+
+
+
+
+
+
