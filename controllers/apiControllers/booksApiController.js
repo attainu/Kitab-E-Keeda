570e43fd77e8865bd4161275f00b496f566c4e76
Amit Pallauri <apallauri@gmail.com>
2020-03-29 04:35:07 +0530

    progress

--- controllers/apiControllers/booksApiController.js
+++ controllers/apiControllers/booksApiController.js
@@ -1,24 +1,43 @@
 const Books = require('../../models/books')
 const fetch = require('node-fetch')
+
 const url = 'https://www.googleapis.com/books/v1/volumes?q'
+const apiKey = 'AIzaSyCzzGVsnUvux0EDsxgzkHGBhk1q8Y3HtdI'
+
+// 'https://www.googleapis.com/books/v1/volumes?q=subject:science&startIndex=0&maxResults=40&key=AIzaSyCzzGVsnUvux0EDsxgzkHGBhk1q8Y3HtdI'
 
 module.exports = {
-    async postGenre(req, res){
-        try{
-            const book = await fetch(`${url}=flowers`)
-            const books = await book.json()
-            res.json(books)        
-        }catch(err){
-            res.json(err)
-        }
-        
-    }
-
-    // async postFavAuthor(req, res){
-
-    // },
-
-    // async postReadBooks(req, res){
-
+    // async postGenre(req, res){
+    //     try{
+    //         const genre = []
+    //         const books = []
+    //         const { genre1, genre2, genre3, genre4, genre5 } = req.headers
+    //         genre.push( genre1, genre2, genre3, genre4, genre5 )
+    //         genre.forEach( el => {
+    //             const bookFetch = await fetch(`${url}=subject:${el}&startIndex=0&maxResults=40&key=${apiKey}`)
+    //             const bookDatas = await bookFetch.json()
+    //             books.push( bookDatas )
+    //         })
+    //         res.json(books)        
+    //     }catch(err){
+    //         res.json(err)
+    //     }
     // }
+     postGenre :  function(req, res){
+        const genre = []
+        const books = []
+        const { genre1, genre2, genre3, genre4, genre5 } = req.headers
+        genre.push( genre1, genre2, genre3, genre4, genre5 )
+        genre.forEach( el => {
+            if(el !== undefined){
+                fetch(`${url}=subject:${el}&startIndex=0&maxResults=10&key=${apiKey}`).then(response => {
+                    return response.json()
+                }).then(bookDatas =>{
+                    books.push( bookDatas )
+                    res.json(books)             // -------------- here the books array is printing with the required data
+                }).catch(err => console.log(err))   
+            }
+        })
+        console.log(books) //---------------- here the books is printing an empty array     
+    }
 }
