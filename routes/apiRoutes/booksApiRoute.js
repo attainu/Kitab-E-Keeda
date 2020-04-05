22ac9a2320af7ccfc8297d2c2d97770f9556d022
Amit Pallauri <apallauri@gmail.com>
2020-04-05 21:47:43 +0530

    added the get routes and controllers for all books

--- routes/apiRoutes/booksApiRoute.js
+++ routes/apiRoutes/booksApiRoute.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
-const { postGenre, postFavAuthor, postReadBooks, addReviews } = require('../../controllers/apiControllers/booksApiController')
+const { postGenre, postFavAuthor, postBooksRead, addReviews } = require('../../controllers/apiControllers/booksApiController')
+const { getAllBooks, sortByGenres, sortByAuthors, sortByBooksRead } = require('../../controllers/apiControllers/getAllBookData')
 const { authenticate } = require('../../middlewares/authenticate')
 
 router.post('/books/genre/:userId', authenticate, postGenre )
 router.post('/books/favAuthor/:userId',authenticate, postFavAuthor )
-router.post('/books/readBooks/:userId',authenticate, postReadBooks )
-router.post('/books/addReview/:userId/:bookId',authenticate, addReviews)
+router.post('/books/readBooks/:userId',authenticate, postBooksRead )
+router.post('/books/addReview/:userId/:bookId',authenticate, addReviews )
+
+router.get('/books/getAllBooks', getAllBooks )
+router.get('/books/sortByGenres/:userID', sortByGenres )
+router.get('/books/sortByAuthors/:userID', sortByAuthors )
+router.get('/user/sortByBooksRead/:userID', sortByBooksRead )
+
 
 module.exports = router
