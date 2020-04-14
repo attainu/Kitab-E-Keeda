b2db73a3691fd1b47d69ea4fc31d3f3391b7883b
Amit Pallauri <apallauri@gmail.com>
2020-04-15 01:41:51 +0530

    added

--- controllers/apiControllers/getAllBookData.js
+++ controllers/apiControllers/getAllBookData.js
@@ -1,6 +1,8 @@
 const Books = require('../../models/books')
 const User = require('../../models/Users')
 const Genres = require('../../models/genres')
+const FavAuthors = require('../../models/favAuthros')
+const BooksRead = require('../../models/booksRead')
 
 module.exports = {
     async getAllBooks(req, res) {
@@ -31,105 +33,79 @@ module.exports = {
 
     async sortByGenres(req, res) { 
        try{
-            Genres.findAll({ where : {}})
+           const { userId } = req.params
+            let genres =[]
+            let bookList = []
+            const foundGenres = await Genres.findAll({ where : {userId}})
+            genres.push(foundGenres)
+            setTimeout(()=>{
+                genres.forEach(genre => {
+                    Books.findAll({where : { categories : genre }}).exec((err, foundBook)=>{
+                        if(err) console.log(err)
+                        bookList.push(foundBook)
+                    })
+                })
+                setTimeout(()=>{
+                    res.send(bookList)
+                }, 2000)            
+            }, 3000)
        }catch(err){
            console.log(err)
        }
-        // var myGenre = []
-        // const { userId } = req.params;
-        // User.find({ _id: userId}).exec((err, data) => {
-        //     if (err) console.log(err)
-        //     console.log(data)
-        //     data[0].genres.forEach(el => {
-        //         myGenre.push(el)
-        //         console.log(el)
-        //     })
-        // })
-        //     setTimeout(() => {
-        //         var favGenreData = []
-        //         myGenre.forEach(ele => {
-        //             Books.find({ "volumeInfo.categories": ele }, {
-        //                 _id: 1,
-        //                 "volumeInfo.title": 1,
-        //                 "volumeInfo.categories": 1,
-        //                 "volumeInfo.imageLinks.smallThumbnail": 1,
-        //                 "volumeInfo.publishedDate": 1,
-        //                 "saleInfo.listPrice.amount": 1
-        //             }).then(doc => favGenreData.push(doc)).catch(err => console.log(err))
-        //         })
-        //         setTimeout(() => {
-        //             res.send(favGenreData)
-        //         }, 2000)
-        //     }, 3000);
     },
 
     async sortByAuthors(req, res) { 
-        const userID = req.params.userId;
-        var myAuthor = []
-        User.find({ _id: userID }, { _id: 0,favAuthors: 1 }).exec((err, data) => {
-            if (err) console.log(err)
-            console.log(data)
-                data[0].favAuthors.forEach(el => {
-                myAuthor.push(el)
-                console.log(el)
-            })
-        })
-
-            setTimeout(() => {
-                var favAuthorData = []
-                myAuthor.forEach(ele => {
-                    Books.find({ "volumeInfo.authors": ele }, {
-                        _id: 1,
-                        "volumeInfo.title": 1,
-                        "volumeInfo.authors": 1,
-                        "volumeInfo.imageLinks.smallThumbnail": 1,
-                        "volumeInfo.publishedDate": 1,
-                        "saleInfo.listPrice.amount": 1
-                    }).then(doc => favAuthorData.push(doc)).catch(err => console.log(err))
-                })
-                setTimeout(() => {
-                    res.send(favAuthorData)
-                }, 2000)
-            }, 3000);
+        try{
+            const { userId } = req.params
+             let authors =[]
+             let bookList = []
+             const foundAuthors = await FavAuthors.findAll({ where : {userId}})
+             genres.push(foundAuthors)
+             setTimeout(()=>{
+                 authors.forEach(author => {
+                    Books.findAll({where : { authors : author }}).exec((err, foundBook)=>{
+                        if(err) console.log(err)
+                        bookList.push(foundBook)
+                    })
+                 })
+                 setTimeout(()=>{
+                     res.send(bookList)
+                 }, 2000)            
+             }, 3000)
+        }catch(err){
+            console.log(err)
+        }
     },
 
     async sortByBooksRead(req, res) {
-        const userID = req.params.userId;
-        var myTitle = []
-        User.find({_id: userID}, {_id: 0,favBooks: 1}).exec((err, data) => {
-            if (err) console.log(err)
-            console.log(data)
-                data[0].favBooks.forEach(el => {
-                myTitle.push(el)
-                console.log(el)
-            })
-        }) 
-            setTimeout(() => {
-                var favBookData = []
-                myTitle.forEach(ele => {
-                    Books.find({
-                        "volumeInfo.title": ele }, {
-                        _id: 1,
-                        "volumeInfo.title": 1,
-                        "volumeInfo.authors": 1,
-                        "volumeInfo.imageLinks.smallThumbnail": 1,
-                        "volumeInfo.publishedDate": 1,
-                        "saleInfo.listPrice.amount": 1
-                    }).then(doc => favBookData.push(doc)).catch(err => console.log(err))
-                })
-                setTimeout(() => {
-                    res.send(favBookData)
-                }, 2000)
-            }, 3000);
+        try{
+            const { userId } = req.params
+             let booksRead =[]
+             let bookList = []
+             const foundBooksRead = await BooksRead.findAll({ where : {userId}})
+             booksRead.push(foundBooksRead)
+             setTimeout(()=>{
+                booksRead.forEach(book => {
+                    Books.findAll({where : { title : book }}).exec((err, foundBook)=>{
+                        if(err) console.log(err)
+                        bookList.push(foundBook)
+                    })
+                 })
+                 setTimeout(()=>{
+                     res.send(bookList)
+                 }, 2000)            
+             }, 3000)
+        }catch(err){
+            console.log(err)
+        }
     },
 
     async getSearchedBook(req, res){
         try{
             const bookId = req.params.bookId
-            Books.find({ _id : bookId }).exec((err, resp)=>{
-                if(err) return res.send(err)
-                res.send(resp)
-            })
+            const book = await Books.findOne({ where : { _id : bookId }})
+            if(!book) return res.send("invalid credentials")
+            res.send(book)
         }catch(err){
             console.log(err)
         }
