9c5b542ba7f849a562a3524ac2e5940014d77723
Manas-Sahoo-au6 <rmanas000@gmail.com>
2020-04-13 20:04:44 -0700

    post controlleer updated

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ app.get('/', (_, res) => {
 
 const userApiRoute = require('./routes/apiRoutes/userApiRoute')
 // const booksApiRoute = require('./routes/apiRoutes/booksApiRoute')
-// const postsApiRoute = require('./routes/apiRoutes/postsApiRoutes')
+const postsApiRoute = require('./routes/apiRoutes/postsApiRoutes')
 
 
 //routes global middlewares
 app.use(userApiRoute);
 // app.use(booksApiRoute);
-// app.use(postsApiRoute);
+app.use(postsApiRoute);
 
 
 
--- controllers/apiControllers/postsApiController.js
+++ controllers/apiControllers/postsApiController.js
@@ -9,9 +9,9 @@ module.exports = {
         try{
             const post = req.body.postBody
             const user = req.params.userId   
-            const newPost = new Post({ post, user })
-            await newPost.save()
-            const foundUser  = await User.findOneAndUpdate({ _id : user }, { $push : { posts : newPost._id } })
+            const newPost = await  Post.create({ post, user })
+            
+            const foundUser  = await User.update( { $push : { posts : newPost._id } },{where:{ _id : user }})
             if(!foundUser) return res.status(400).send("invalid credentials")
             res.json(newPost)    
         }catch(err){
@@ -23,9 +23,9 @@ module.exports = {
         try{
             const comment = req.body.commentBody;
             const { userId, postId } = req.params
-            const newComment = new Comment({ comment, userId, postId })
-            await newComment.save()
-            const foundPost = await Post.findOneAndUpdate({ _id : postId }, { $push : { comments : newComment._id }})
+            const newComment =  await Comment.create({ comment, userId, postId })
+            
+            const foundPost = await Post.update( { $push : { comments : newComment._id }},{where:{ _id : postId }})
             console.log(foundPost)
             if(!foundPost) return res.status(400).send("invalid credentials")
             res.send(newComment)
@@ -38,23 +38,23 @@ module.exports = {
         try{
             const { userId, postId } = req.params
             const { like } = req.headers
-            const newLike = new Like({ userId, postId, like })
+            const newLike =  Like.build({ userId, postId, like })
             // to add likes count in posts
             if(newLike.like === true ){
                 newLike.dislike = false
-                Post.findOneAndUpdate({ _id : postId }, {$inc : { likesCount : 1 }}).exec((err, resp) => {
+                Post.update( {$inc : { likesCount : 1 }},{where:{ _id : postId }}).exec((err, resp) => {
                     if(err) console.log(err)
                     console.log(resp)
                 })            
             }else{
-                Post.findOneAndUpdate({ _id : postId }, {$inc : { disLikesCount : 1 }}).exec((err, resp) => {
+                Post.update( {$inc : { disLikesCount : 1 }},{where:{ _id : postId }}).exec((err, resp) => {
                     newLike.dislike = true
                     if(err) console.log(err)
                     console.log(resp)
                 }) 
             }
             await newLike.save()
-            Post.findOneAndUpdate({ _id : postId }, { $push : { likes : newLike._id}}).exec((err, resp) => {
+            Post.update( { $push : { likes : newLike._id}},{where:{ _id : postId }}).exec((err, resp) => {
                 if(err) console.log(err)
                 console.log(resp)
             })
@@ -68,9 +68,9 @@ module.exports = {
         try{
             const { userId, commentId } = req.params
             const thread  = req.body.threadBody
-            const newThread = new Thread({ userId, commentId, thread })
-            await newThread.save()
-            const foundComment = await Comment.findOneAndUpdate({ _id : commentId }, { $push : { threadId : newThread._id }})
+            const newThread = await Thread.create({ userId, commentId, thread })
+            
+            const foundComment = await Comment.update( { $push : { threadId : newThread._id }},{where:{ _id : commentId }})
             if(!foundComment) return res.send("invalid Credentials")
             res.json(newThread)
         }catch(err){
@@ -81,12 +81,12 @@ module.exports = {
     async getPosts(req, res){
         try {
             const { userId } = req.params
-            const foundUser = await User.findOne({ _id : userId })
+            const foundUser = await User.find({where:{ _id : userId }})
             const followingUsers = foundUser.followingUser
             setTimeout(() => {
                 const userPosts = []
                 followingUsers.forEach(el => {
-                     Post.find({ user: el }).then(doc =>{
+                     Post.find({where:{ user: el }}).then(doc =>{
                          if(!doc) console.log(err)
                          else userPosts.push(doc)                     
                         }).catch(err => console.log(err))
@@ -104,7 +104,7 @@ module.exports = {
         try{
             const { userId, postId } = req.params
             const { post } = req.headers
-            const foundPost = await Post.findOneAndUpdate({ _id : postId, user : userId }, { post })
+            const foundPost = await Post.update( { post:post },{where:{ _id : postId, user : userId }})
             if(!foundPost) return res.status(400).send("invalid credentials")
             res.json({msg : "post updated successfully", foundPost })
         }catch(err){
@@ -115,15 +115,15 @@ module.exports = {
     async deletePosts(req, res){
         try{
             const { userId, postId } = req.params
-            const foundPost = await Post.findOneAndDelete({ _id : postId, user : userId })
+            const foundPost = await Post.destroy({ _id : postId, user : userId })
             if(!foundPost) return res.status(400).send("invalid credentials")
             else if(foundPost !== null ) res.json({msg : "post deleted successfully", foundPost })
             else return res.status(200).send("nothing to delete")
-            Comment.deleteMany({ postId, userId }).exec((err, _)=>{
+            Comment.destroy({ postId: postId, userId: userId }).exec((err, _)=>{
                 if(err) console.log(err.message)
                 console.log("comment related to post deleted")
             })
-            Like.deleteMany({ postId, userId }).exec((err, _)=>{
+            Like.destroy({ postId: postId, userId: userId }).exec((err, _)=>{
                 if(err) console.log(err.message)
                 console.log("likes related to the post has been deleted")
             })
@@ -136,7 +136,7 @@ module.exports = {
         try{
             const { userId, commentId } = req.params
             const { comment } = req.headers
-            const foundComment = await Comment.findOneAndUpdate({ _id : commentId, userId }, { comment })
+            const foundComment = await Comment.update( { comment:comment },{where:{ _id : commentId,userId: userId }})
             if(!foundComment) return res.status(400).send("invalid credntials")
             res.json({ msg : "comment updated successfully ", foundComment })
         }catch(err){
@@ -147,11 +147,11 @@ module.exports = {
     async deleteComments(req, res){
         try{
             const { userId, commentId } = req.params
-            const foundComment = await Comment.findOneAndDelete({ _id : commentId })
+            const foundComment = await Comment.destroy({ _id : commentId })
             if(!foundComment) return res.send("invalid credentials")
             else if(foundComment !== null ) res.json({msg:"comment deleted successfully", foundComment})
             else return res.status(200).send("nothing to delete")
-            Thread.deleteMany({ commentId, userId }).exec((err, _)=>{
+            Thread.destroy({ commentId:commentId, userId: userId }).exec((err, _)=>{
                 if(err) console.log(err)
                 console.log("threads under the comment deleted successfully")
             })
@@ -164,7 +164,7 @@ module.exports = {
         try{
             const { userId, threadId } = req.params
             const { thread } = req.headers
-            const foundThread = await Thread.findOneAndUpdate({ _id : threadId ,userId }, { thread})
+            const foundThread = await Thread.update( { thread :thread},{where:{ _id : threadId ,userId :userId}})
             if(!foundThread) return res.send("invalid credentials")
             res.json({ msg: "thread updated successfully", foundThread })
         }catch(err){
@@ -175,7 +175,7 @@ module.exports = {
     async deleteThreads(req, res){
         try{
             const { userId, threadId } = req.params
-            Thread.findByIdAndDelete({ _id : threadId, userId }).exec((err, resp)=>{
+            Thread.destroy({ _id : threadId, userId : userId }).exec((err, resp)=>{
                 if(err) return res.send("invalid credentials")
                 else if(resp !== null) return res.json({msg : "thread deleted successfully", resp})
                 else return res.send("nothing to delete") 
